5699405061203a5bd5c9dc0ed9e76c5e57aa7ae0
gitalf96 <126389189+gitalf96@users.noreply.github.com>
2024-05-06 13:05:44 +0530

    Update PatientCondition_prediction.py

--- PatientCondition_prediction.py
+++ PatientCondition_prediction.py
@@ -63,8 +63,8 @@ st.markdown(page_element, unsafe_allow_html=True)
 st.markdown("""
 <style>
 div[data-testid="column"] {
-   background-color: rgba(204, 204, 255, 0.9);
-   # background-color: rgba(0,0,0, 0.9);
+   # background-color: rgba(204, 204, 255, 0.9);
+   background-color: rgba(0,0,0, 0.9);
    border: 3px solid rgba(0,0,0, 0.0);
    padding: 3% 2% 3% 3%;
    border-radius:4px;
